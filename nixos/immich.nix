3c72ae13bb30553d2b7e550bca40ba156e6b9406
GabrielDTB <gabriel@gabrieltb.me>
2023-07-13 13:59:59 -0700

    add immich

--- nixos/immich.nix
+++ nixos/immich.nix
@@ -1,19 +1,17 @@
 { pkgs, ... }:
 
 {
-  services.btrbk = {
-    instances.immich = {
+  services.btrbk.instances = {
+    immich = {
       onCalendar = "*-*-* *:*:00";
       settings = {
         timestamp_format = "long-iso";
         snapshot_preserve_min = "18h";
         snapshot_preserve = "36h 365d";
-        volume = {
-          "/immich" = {
-            snapshot_dir = ".snapshots";
-            subvolume = "pictures";
-            snapshot_create = "onchange";
-          };
+        volume."/immich" = {
+          snapshot_dir = ".snapshots";
+          subvolume."data/library".snapshot_name = "library";
+          snapshot_create = "onchange";
         };
       };
     };
@@ -24,10 +22,14 @@
       fsType = "btrfs";
       options = [ "subvol=@immich" "noatime" ];
     };
-    "/immich/pictures" = pkgs.lib.mkForce {
+    "/immich/data/library" = pkgs.lib.mkForce {
       device = "/dev/sda1";
       fsType = "btrfs";
       options = [ "subvol=@pictures" "noatime" ];
     };
   };
+  virtualisation.docker.enable = true;
+  environment.systemPackages = with pkgs; [
+    docker-compose
+  ];
 }
