729150776b5773cb47b95b33e511263e8dd4fcbe
Gabriel Talbert Bunt <gabriel@gabrieltb.me>
2024-07-31 05:09:33 -0400

    hardware configuration

--- hosts/gbox/hardware-configuration.nix
+++ hosts/gbox/hardware-configuration.nix
@@ -15,6 +15,7 @@
   boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod"];
   boot.initrd.kernelModules = [];
   boot.kernelModules = ["kvm-amd" "wl"];
+  boot.kernelParams = [ "quiet" ];
   boot.extraModulePackages = [config.boot.kernelPackages.broadcom_sta];
 
   # networking.interfaces.enp5s0.useDHCP = lib.mkDefault true;
@@ -22,4 +23,5 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.enableAllFirmware = true;
 }
