2ff59dffdedd87d5d86241a92ac635d44dae3ac4
Gabriel Talbert Bunt <gabriel@gabrieltb.me>
2025-02-21 12:21:29 -0500

    save remote builders attempt

---
+++ users/gabe/builders/nixos.nix
@@ -0,0 +1,26 @@
+{...}: {
+  nix = {
+    buildMachines = [{
+      hostName = "gbox";
+      sshUser = "gabe";
+      system = "x86_64-linux";
+      sshKey = "/home/gabe/.ssh/id_ed25519";
+      protocol = "ssh-ng";
+      supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
+      # maxJobs = 1;
+      # speedFactor = 1;
+    }];
+    distributedBuilds = true;
+    extraOptions = ''
+      builders-use-substitutes = true
+    '';
+    # settings = {
+    #   trusted-public-keys = [
+    #     "builder-cache:p97XQWA2Oo66nK/PcKz0q1U64dUw0FkuHLdo+y4jxQI="
+    #   ];
+    #   substituters = [
+    #     "ssh-ng://builder"
+    #   ];
+    # };
+  };
+}
