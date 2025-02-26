a6c5c0d49d57a99e2f0f6c04ed5cf490ef989c56
Gabriel Talbert Bunt <gabriel@gabrieltb.me>
2025-02-25 23:14:17 -0500

    enable automatic gc

--- modules/nix/shared.nix
+++ modules/nix/shared.nix
@@ -16,5 +16,10 @@
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       ];
     };
+    gc = {
+      automatic = true;
+      options = "--delete-older-than 60d";
+    };
+    # optimise.automatic = true;
   };
 }
