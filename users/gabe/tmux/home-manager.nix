20b1e1b333d644eedcec37a8c126ee429bd5688e
Gabriel Talbert Bunt <gabriel@gabrieltb.me>
2025-03-31 11:37:58 -0400

    fix tmux + helix space highlighting

--- users/gabe/tmux/home-manager.nix
+++ users/gabe/tmux/home-manager.nix
@@ -2,5 +2,9 @@
   programs.tmux = {
     enable = true;
     escapeTime = 0;
+    extraConfig = ''
+      set -g default-terminal "tmux-256color"
+      set -ag terminal-overrides ",xterm-256color:RGB"
+    '';
   };
 }
