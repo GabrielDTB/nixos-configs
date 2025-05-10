83c7dcceaa817828295fab0648e7c81f55de6523
Gabriel Talbert Bunt <gabriel@gabrieltb.me>
2025-05-09 20:49:37 -0400

    add gnome

---
+++ users/gabe/gnome/home-manager.nix
@@ -0,0 +1,22 @@
+{
+  pkgs,
+  ...
+}:
+{
+  programs.gnome-shell = {
+    extensions = with pkgs.gnomeExtensions; [
+      blur-my-shell
+      launch-new-instance
+      space-bar
+      forge
+    ];
+  };
+  dconf = {
+    enable = true;
+    settings = {
+      # "org/gnome/desktop/interface" = {
+      #   clock-show-weekday = true;
+      # };
+    };
+  };
+}
