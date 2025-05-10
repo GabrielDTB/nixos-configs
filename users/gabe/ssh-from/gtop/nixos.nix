6ed48eebb5793d256299d3fa6c2b920741c8d976
Gabriel Talbert Bunt <gabriel@gabrieltb.me>
2025-05-09 22:24:45 -0400

    add gfrm to authorized ssh keys

--- users/gabe/ssh-from/gtop/nixos.nix
+++ users/gabe/ssh-from/gtop/nixos.nix
@@ -1,7 +1,8 @@
 {...}: {
   users.users.gabe = {
     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG8Aa9Q+Wf1YFi0XqG/5OK0RruJL7ItRQKLQtMVUsIIn gabriel@gabrieltb.me"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG8Aa9Q+Wf1YFi0XqG/5OK0RruJL7ItRQKLQtMVUsIIn gabe@gtop"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB23lwFYWkeI2AOE5AvnMCSYGw1IFViidf08QRRScDXc gabe@gfrm"
     ];
   };
 }
