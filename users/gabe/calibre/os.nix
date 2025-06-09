81227eadef4e0f9cf67ec4a0888a4416b8c08094
Gabriel Talbert Bunt <gabriel@gabrieltb.me>
2025-06-08 23:10:24 -0400

    enable kobo integration in calibre-web

--- users/gabe/calibre/os.nix
+++ users/gabe/calibre/os.nix
@@ -1,12 +1,20 @@
 {
+  pkgs,
   ...
 }: {
   services.calibre-web = {
     enable = true;
+    package = (pkgs.calibre-web.overridePythonAttrs (old: {
+      dependencies =
+        with old.optional-dependencies;
+          old.dependencies ++ kobo ++ metadata ++ comics;
+    }));
     options = {
       enableBookUploading = true;
       enableBookConversion = true;
       enableKepubify = true;
     };
+    listen.ip = "0.0.0.0";
+    openFirewall = true;
   };
 }
