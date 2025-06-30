7a8ee1a147bfa98f28ae11892a7ce52feabb3fbb
Gabriel Talbert Bunt <gabriel@gabrieltb.me>
2025-06-29 20:31:45 -0400

    pin qbittorrent to v5.1.0
    find a better way to do this later

--- users/gabe/qbittorrent/home.nix
+++ users/gabe/qbittorrent/home.nix
@@ -1,5 +1,15 @@
-{pkgs, ...}: {
-  home.packages = with pkgs; [
-    qbittorrent
+{
+  pkgs,
+  config,
+  ...
+}: let
+  pinnedQB =
+    (import (builtins.fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/archive/62b852f6c.tar.gz";
+      sha256 = "04c6dkshw07bm2isv7rvl6xgr4hn7hqznr0v2kww6zjfz4awk4a7";
+    }) {system = config.nixpkgs.system;}).qbittorrent;
+in {
+  home.packages = [
+    pinnedQB
   ];
 }
