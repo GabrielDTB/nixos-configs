1ff4bfd4b40a37b1b2e47e3b70380a6517389920
Gabriel Talbert Bunt <gabriel@gabrieltb.me>
2025-07-13 15:44:08 -0400

    add ssh-from gbox

---
+++ users/gabe/ssh-from/gbox/os.nix
@@ -0,0 +1,7 @@
+{...}: {
+  users.users.gabe = {
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILHEs4BLfJ9vniDJhrqcndDijv6ZIdUreZf2e+vQxGGa gabe@gbox"
+    ];
+  };
+}
