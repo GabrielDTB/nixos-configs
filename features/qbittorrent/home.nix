b53db840f895932ba9a447eb32150a9e4d71a31f
Gabriel Talbert Bunt <gitcommit@dendry.net>
2026-03-07 13:21:50 -0500

    migrate to stdenv.hostPlatform.system
    I still get the warning, with no idea what causes it.
    Oh well.

--- features/qbittorrent/home.nix
+++ features/qbittorrent/home.nix
@@ -7,7 +7,7 @@
   #   (import (builtins.fetchTarball {
   #     url = "https://github.com/NixOS/nixpkgs/archive/62b852f6c.tar.gz";
   #     sha256 = "04c6dkshw07bm2isv7rvl6xgr4hn7hqznr0v2kww6zjfz4awk4a7";
-  #   }) {system = config.nixpkgs.system;}).qbittorrent;
+  #   }) {system = config.nixpkgs.stdenv.hostPlatform.system;}).qbittorrent;
 in {
   home.packages = with pkgs; [
     # pinnedQB
