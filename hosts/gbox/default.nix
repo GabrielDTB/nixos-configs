4457b96ffdbd492b7eb82571dd2a802706829fbd
Gabriel Talbert Bunt <gitcommit@dendry.net>
2026-04-02 22:33:39 -0400

    enable static ip

--- hosts/gbox/default.nix
+++ hosts/gbox/default.nix
@@ -111,10 +111,22 @@
     ];
   };
 
-  networking.extraHosts = ''
-    127.0.0.1 youtube.com
-    127.0.0.1 www.youtube.com
-  '';
+  networking = {
+    interfaces = {
+      enp5s0.ipv4.addresses = [{
+        address = "192.168.40.101";
+        prefixLength = 24;
+      }];
+    };
+    defaultGateway = {
+      address = "192.168.40.1";
+      interface = "enp5s0";
+    };
+    extraHosts = ''
+      127.0.0.1 youtube.com
+      127.0.0.1 www.youtube.com
+    '';
+  };
 
   # boot.extraModprobeConfig = ''
   #   blacklist nouveau
