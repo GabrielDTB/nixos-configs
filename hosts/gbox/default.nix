a6415a7ba7e2a9426a5328a5ac4dea51fafe6dee
Gabriel Talbert Bunt <gitcommit@dendry.net>
2026-06-23 11:14:10 -0400

    back to dhcp

--- hosts/gbox/default.nix
+++ hosts/gbox/default.nix
@@ -116,16 +116,20 @@
   services.lact.enable = true;
 
   networking = {
-    interfaces = {
-      enp5s0.ipv4.addresses = [{
-        address = "192.168.40.101";
-        prefixLength = 24;
-      }];
-    };
-    defaultGateway = {
-      address = "192.168.40.1";
-      interface = "enp5s0";
-    };
+    # interfaces = {
+    #   enp5s0.ipv4.addresses = [{
+    #     address = "192.168.40.101";
+    #     prefixLength = 24;
+    #   }];
+    # };
+    # defaultGateway = {
+    #   address = "192.168.40.1";
+    #   interface = "enp5s0";
+    # };
+    # extraHosts = ''
+    #   127.0.0.1 youtube.com
+    #   127.0.0.1 www.youtube.com
+    # '';
   };
 
   # boot.extraModprobeConfig = ''
