64c612d95d545f253acd53c6204fdf7b75b5c470
Gabriel Talbert Bunt <gitcommit@dendry.net>
2026-09-16 14:57:37 -0400

    merge attrsets properly

--- modules/nix/home-manager.nix
+++ modules/nix/home-manager.nix
@@ -2,10 +2,10 @@
   lib,
   inputs,
   ...
-}:
-{
+}: {
+  imports = [(import ./shared.nix {inherit lib inputs;})];
+
   home.sessionVariables.NIX_PATH = lib.concatStringsSep ":" (
     lib.mapAttrsToList (key: value: "${key}=${value}") inputs
   );
 }
-// (import ./shared.nix {inherit lib inputs;})
--- modules/nix/nixos.nix
+++ modules/nix/nixos.nix
@@ -2,8 +2,8 @@
   lib,
   inputs,
   ...
-}:
-{
+}: {
+  imports = [(import ./shared.nix {inherit lib inputs;})];
+
   nix.nixPath = lib.mapAttrsToList (key: value: "${key}=${value}") inputs;
 }
-// (import ./shared.nix {inherit lib inputs;})
